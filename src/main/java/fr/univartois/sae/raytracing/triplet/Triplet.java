de993d9e86e72eb3ce5904cafeef8ea943b15be7
Leo Denis <leo_denis@ens.univ-artois.fr>
2023-10-23 12:52:08 +0200

    :construction: Début de la classe Triplet

---
+++ src/main/java/fr/univartois/sae/raytracing/triplet/Triplet.java
@@ -0,0 +1,25 @@
+package fr.univartois.sae.raytracing.triplet;
+
+public class Triplet {
+    private double x;
+    private double y;
+    private double z;
+
+    public Triplet(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+}
