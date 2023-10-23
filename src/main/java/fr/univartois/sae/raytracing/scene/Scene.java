d54be5d29c3a1d151a2b6ff8002b8037de097086
Matheo Dupuis <matheo_dupuis@ens.univ-artois.fr>
2023-10-23 16:12:32 +0200

    :construction: Changement de camera pour une arrayList

--- src/main/java/fr/univartois/sae/raytracing/scene/Scene.java
+++ src/main/java/fr/univartois/sae/raytracing/scene/Scene.java
@@ -5,13 +5,14 @@ import fr.univartois.sae.raytracing.triplet.Triplet;
 import fr.univartois.sae.raytracing.object.AObject;
 import fr.univartois.sae.raytracing.light.Light;
 
+import java.util.ArrayList;
 
 
 public class Scene {
 
     private int width;
     private int height;
-    private Triplet[] camera;
+    private ArrayList<Triplet> camera;
     private Light[] lights;
     private Color[] colors;
     private AObject[] objects;
