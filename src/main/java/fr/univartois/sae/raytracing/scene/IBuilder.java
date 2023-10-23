8cf007e04edc9697fe9038da4666f306426a6bd4
Matheo Dupuis <matheodupuis1@gmail.com>
2023-10-23 20:57:06 +0200

    :pencil2: Doc pour l'interface IBuilder

--- src/main/java/fr/univartois/sae/raytracing/scene/IBuilder.java
+++ src/main/java/fr/univartois/sae/raytracing/scene/IBuilder.java
@@ -9,13 +9,45 @@ import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
 
+/**
+ * @author matheo.dupuis
+ *
+ * This Interface is used to build a scene
+ */
 public interface IBuilder {
 
+    /**
+     * Set the values of width and height
+     * @param width
+     * @param height
+     */
     public void buildScene(int width, int height);
+
+    /**
+     * Set the settings for the camera
+     * @param lookFrom
+     * @param lookAt
+     * @param up
+     */
     public void buildCamera(Triplet lookFrom, Triplet lookAt, Triplet up);
+
+    /**
+     * Set the lights list
+     * @param lights
+     */
     public void buildLight(ArrayList<Light> lights);
-    public void buildObject(ArrayList<AObject> object);
-    public void buildColors(JSONObject color);
+
+    /**
+     * Set the object list
+     * @param objects
+     */
+    public void buildObject(ArrayList<AObject> objects);
+
+    /**
+     * Set the values of the colors inside color list using a JSONObject
+     * @param colors
+     */
+    public void buildColors(JSONObject colors);
 
 
 }
