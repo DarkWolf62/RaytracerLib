a53e20377a50c7f4b9876e76983a816401ab0fe9
Matheo Dupuis <matheo_dupuis@ens.univ-artois.fr>
2023-10-23 16:48:18 +0200

    :construction: Création de la classe SceneBuilder

---
+++ src/main/java/fr/univartois/sae/raytracing/scene/SceneBuilder.java
@@ -0,0 +1,25 @@
+package fr.univartois.sae.raytracing.scene;
+
+public class SceneBuilder implements IBuilder{
+
+
+    @Override
+    public void buildScene() {
+
+    }
+
+    @Override
+    public void buildLight() {
+
+    }
+
+    @Override
+    public void buildObject() {
+
+    }
+
+    @Override
+    public void buildColors() {
+
+    }
+}
