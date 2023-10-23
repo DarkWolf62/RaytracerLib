f0674f9e9edbf4fd285ac6f2afb352ed987536f2
Matheo Dupuis <matheodupuis1@gmail.com>
2023-10-23 20:42:25 +0200

    :pencil2: Doc pour la classe Scene

--- src/main/java/fr/univartois/sae/raytracing/scene/Scene.java
+++ src/main/java/fr/univartois/sae/raytracing/scene/Scene.java
@@ -8,6 +8,11 @@ import fr.univartois.sae.raytracing.light.Light;
 import java.util.ArrayList;
 
 
+/**
+ * @author matheo.dupuis
+ *
+ * this class represents the scene
+ */
 public class Scene {
 
     private int width;
@@ -18,6 +23,16 @@ public class Scene {
     private ArrayList<AObject> objects;
 
 
+    /**
+     * Constructor using image's dimension, camera's information, lights, colors, objects
+     *
+     * @param width
+     * @param height
+     * @param camera
+     * @param lights
+     * @param colors
+     * @param objects
+     */
     public Scene(int width, int height, ArrayList<Triplet> camera, ArrayList<Light> lights, ArrayList<Color> colors, ArrayList<AObject> objects) {
         this.width = width;
         this.height = height;
@@ -27,50 +42,98 @@ public class Scene {
         this.objects = objects;
     }
 
+    /**
+     * Getter for the width
+     * @return width
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * Setter for the width
+     * @param width
+     */
     public void setWidth(int width) {
         this.width = width;
     }
 
+    /**
+     * Getter for height
+     * @return height
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * Setter for height
+     * @param height
+     */
     public void setHeight(int height) {
         this.height = height;
     }
 
+    /**
+     * Getter for camera
+     * @return camera
+     */
     public ArrayList<Triplet> getCamera() {
         return camera;
     }
 
+    /**
+     * Setter for camera
+     * @param camera
+     */
     public void setCamera(ArrayList<Triplet> camera) {
         this.camera = camera;
     }
 
+    /**
+     * Getter lights
+     * @return lights
+     */
     public ArrayList<Light> getLights() {
         return lights;
     }
 
+    /**
+     * Setter for lights
+     * @param lights
+     */
     public void setLights(ArrayList<Light> lights) {
         this.lights = lights;
     }
 
+    /**
+     * Getter for colors
+     * @return colors
+     */
     public ArrayList<Color> getColors() {
         return colors;
     }
 
+    /**
+     * Setter for colors
+     * @param colors
+     */
     public void setColors(ArrayList<Color> colors) {
         this.colors = colors;
     }
 
+    /**
+     * Getter for objects
+     * @return objects
+     */
     public ArrayList<AObject> getObjects() {
         return objects;
     }
 
+    /**
+     * Setter for objects
+     * @param objects
+     */
     public void setObjects(ArrayList<AObject> objects) {
         this.objects = objects;
     }
