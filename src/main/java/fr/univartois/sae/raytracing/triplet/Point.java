eb533340aa1e98c46ca30c17a00d8b5bfecf687a
nicolas_nourry <nicolasnourry61@gmail.com>
2023-10-23 14:27:38 +0200

    documentation de la classe Point

--- src/main/java/fr/univartois/sae/raytracing/triplet/Point.java
+++ src/main/java/fr/univartois/sae/raytracing/triplet/Point.java
@@ -1,22 +1,56 @@
 package fr.univartois.sae.raytracing.triplet;
+/**
+ * @author nicolas
+ *
+ * This class represents a point.
+ */
 
 public class Point {
     private Triplet triplet;
 
+    /**
+     * Constructor for create a point with a triplet
+     * @param triplet is a triplet
+     */
+
     public Point(Triplet triplet){
         this.triplet=triplet;
     }
+
+    /**
+     * Constructor for create a point with coordinate
+     * @param x,y,z is a triplet
+     */
+
     public Point(int x,int y,int z){
         triplet=new Triplet(x,y,z);
     }
+    /**
+     * this method subtraction the triplet of this class and a other triplet
+     * @param triplet
+     * @return a new vector
+     */
+
     public Vector subtraction(Triplet triplet){
 
         return new Vector(this.triplet.subtraction(triplet));
     }
+    /**
+     * this method use the scalar multiplication on the triplet
+     * @param d
+     * @return a new point
+     */
+
     public Point scalarMultiplication(double d){
         return new Point(triplet.scalarMultiplication(d));
 
     }
+
+    /**
+     *
+     * @return this triplet of point
+     */
+
     public Triplet getTriplet(){
         return triplet;
     }
