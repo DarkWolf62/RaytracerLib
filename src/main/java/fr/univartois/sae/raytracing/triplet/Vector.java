ec4d688ae8c30dc403633cacc6d2abe01d5d331e
Matheo Dupuis <matheo_dupuis@ens.univ-artois.fr>
2023-10-23 13:43:52 +0200

    :construction: début de la javadoc de Vector

--- src/main/java/fr/univartois/sae/raytracing/triplet/Vector.java
+++ src/main/java/fr/univartois/sae/raytracing/triplet/Vector.java
@@ -1,17 +1,38 @@
 package fr.univartois.sae.raytracing.triplet;
 
+/**
+ * @author matheo.dupuis
+ *
+ *This class represents a vector
+ */
 public class Vector {
 
     private Triplet triplet;
 
+    /**
+     * Constructor using coordinates
+     * @param x
+     * @param y
+     * @param z
+     */
     public Vector(double x, double y, double z){
         this.triplet = new Triplet(x,y,z);
     }
 
+    /**
+     * Constructor using a triplet
+     * @param triplet
+     */
     public Vector(Triplet triplet){
         this.triplet = triplet;
     }
 
+
+    /**
+     * addition of 2 vectors using a triplet
+     * @param triplet
+     * @return
+     */
     public Vector addition(Triplet triplet){
         return null;
     }
