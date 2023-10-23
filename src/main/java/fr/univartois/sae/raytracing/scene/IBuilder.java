f092cf185389fc146e2cd3e5e0a3c07bcfba0b28
Matheo Dupuis <matheo_dupuis@ens.univ-artois.fr>
2023-10-23 16:29:21 +0200

    :construction: Ajout de l'interface IBuilder

---
+++ src/main/java/fr/univartois/sae/raytracing/scene/IBuilder.java
@@ -0,0 +1,11 @@
+package fr.univartois.sae.raytracing.scene;
+
+public interface IBuilder {
+
+    public void buildScene();
+    public void buildLight();
+    public void buildObject();
+    public void buildColors();
+
+
+}
