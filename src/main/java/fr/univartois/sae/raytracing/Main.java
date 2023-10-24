95c7693520dc498f28cd9391e8e5feb8a5d2d6b1
Matheo Dupuis <matheo_dupuis@ens.univ-artois.fr>
2023-10-24 09:49:52 +0200

    :hammer:  Ajout du fichier test.txt pour la config

--- src/main/java/fr/univartois/sae/raytracing/Main.java
+++ src/main/java/fr/univartois/sae/raytracing/Main.java
@@ -1,7 +1,11 @@
 package fr.univartois.sae.raytracing;
 
+import fr.univartois.sae.raytracing.parser.Parser;
+import fr.univartois.sae.raytracing.scene.IBuilder;
+import fr.univartois.sae.raytracing.scene.SceneBuilder;
+
 public class Main {
     public static void main(String[] args) {
-
+        Parser parser = new Parser("test.txt");
     }
 }
