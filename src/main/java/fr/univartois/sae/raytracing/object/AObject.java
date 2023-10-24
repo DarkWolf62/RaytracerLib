41c2bb0482d8fcdb16d9132d65a9234662afd495
Leo Denis <leo_denis@ens.univ-artois.fr>
2023-10-24 16:36:28 +0200

    :construction: Classe RayTracing à tester plus tard

--- src/main/java/fr/univartois/sae/raytracing/object/AObject.java
+++ src/main/java/fr/univartois/sae/raytracing/object/AObject.java
@@ -1,8 +1,9 @@
 package fr.univartois.sae.raytracing.object;
 
+import fr.univartois.sae.raytracing.triplet.Point;
+
 /**
  *  this class represent an object
  */
 public abstract class AObject {
-
 }
