2a7e911fc150e087e0a0ee48941f80964b88e7c2
Matheo Dupuis <matheodupuis1@gmail.com>
2023-10-25 22:18:20 +0200

    :memo: JavaDoc pour l'interface IStrategy

--- src/main/java/fr/univartois/sae/raytracing/light/IStrategy.java
+++ src/main/java/fr/univartois/sae/raytracing/light/IStrategy.java
@@ -6,7 +6,21 @@ import fr.univartois.sae.raytracing.triplet.Color;
 import fr.univartois.sae.raytracing.triplet.Point;
 import fr.univartois.sae.raytracing.triplet.Vector;
 
+/**
+ * @author matheo.dupuis
+ *
+ * This interface is used to implement a design pattern strategy to choose between the basic model and the Lambert model
+ */
 public interface IStrategy {
 
+    /**
+     * The method the class who implement the interface will have to do
+     *
+     * @param object
+     * @param idObj
+     * @param p
+     * @param scene
+     * @return The color of a pixel
+     */
     public Color modelMethod(Sphere object, int idObj , Point p, Scene scene);
 }
