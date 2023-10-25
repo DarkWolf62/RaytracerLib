7a1b5548bc0e7179e28eeb82ed6fd399b77e56c2
Matheo Dupuis <matheodupuis1@gmail.com>
2023-10-25 23:21:21 +0200

    :memo: JavaDoc pour la classe Main

--- src/main/java/fr/univartois/sae/raytracing/Main.java
+++ src/main/java/fr/univartois/sae/raytracing/Main.java
@@ -6,32 +6,39 @@ import fr.univartois.sae.raytracing.light.LambertModel;
 import fr.univartois.sae.raytracing.parser.Parser;
 import fr.univartois.sae.raytracing.scene.Scene;
 import fr.univartois.sae.raytracing.scene.SceneBuilder;
-import fr.univartois.sae.raytracing.triplet.Color;
 import fr.univartois.sae.raytracing.object.Sphere;
 
-import java.net.URL;
-import java.util.ArrayList;
 
 
 /**
  * @author matheo.dupuis
+ * @author nicolas.nourry
+ * @author nicolas.blart
+ * @author leo.denis
  *
- * This is the main class
- *
+ * This is the main class used to run the programm
  */
 public class Main {
     public static void main(String[] args){
+        // Creation of the parser and construction of the scene
         Parser parser = new Parser("src/main/resources/fr.univartois.sae.raytracing/test.txt");
         parser.construct();
         SceneBuilder sceneBuilder = (SceneBuilder) parser.getBuilder();
         Scene scene = sceneBuilder.getResult();
+
+        // We print the scene to see the informations
         System.out.println(scene.toString());
+
+        //Initialization of the strategy used, we create a new strategy and check if there are some diffused colors to use the Lambert method
+        // Else we use the basic Method
         IStrategy strategy;
         Sphere s = (Sphere) (scene.getObjects().get(0));
         if (s.getColor() == null)
             strategy = new BasicModel();
         else
             strategy = new LambertModel();
+
+        //Creation of a RayTracing object who generate the image thanks to his constructor
         RayTracing r = new RayTracing(scene, strategy);
     }
 }
