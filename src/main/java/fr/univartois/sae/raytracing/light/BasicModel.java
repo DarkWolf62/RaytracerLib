89eefe7a76ffc19482a671c8842bdb481bb72260
Matheo Dupuis <matheodupuis1@gmail.com>
2023-10-25 21:28:53 +0200

    :memo: JavaDoc pour la classe BasicModel

--- src/main/java/fr/univartois/sae/raytracing/light/BasicModel.java
+++ src/main/java/fr/univartois/sae/raytracing/light/BasicModel.java
@@ -9,10 +9,20 @@ import fr.univartois.sae.raytracing.triplet.Vector;
 /**
  * @author matheo.dupuis
  *
- * This class is a part of  represents
+ * This class is a part of the design pattern strategy and represents the basic method to apply
+ * The class implements interface IStrategy
  */
 public class BasicModel implements IStrategy{
 
+    /**
+     * This method returns the color of a pixel using the basic method
+     *
+     * @param object
+     * @param idObj
+     * @param p
+     * @param scene
+     * @return The color of a pixel using the basic method
+     */
     @Override
     public Color modelMethod(Sphere object, int idObj , Point p, Scene scene) {
         Color color = (Color) scene.getColors().get("ambient");
